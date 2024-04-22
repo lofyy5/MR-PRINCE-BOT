dea15460e8b4cb6a313f8ca8b2501e8b253d5fe8
kol2351 <167507528+kol2351@users.noreply.github.com>
2024-04-22 09:45:00 +0300

    Update and rename xxfx.js to fx.js

--- plugins/fx.js
+++ plugins/fx.js
@@ -74,7 +74,7 @@ let handler = async (m, { conn, args, text, usedPrefix, command }) => {
   }
 };
 
-handler.help = ['xnxx']; 
+handler.help = ['s']; 
 handler.tags = ['nsfw', 'prem'];
 handler.command = ['xnxxsearch', 'xnxxdl', 'xxx']; 
 handler.diamond = false;
